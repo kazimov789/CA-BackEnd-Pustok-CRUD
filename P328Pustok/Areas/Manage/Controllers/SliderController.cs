a1e791b0915b73e4765eb0333a60c513663dcbe8
kazimov789 <118839541+kazimov789@users.noreply.github.com>
2023-05-18 01:23:43 +0400

    Pustok CRUD For Author,Tag,Genre

---
+++ P328Pustok/Areas/Manage/Controllers/SliderController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P328Pustok.DAL;
+using P328Pustok.Models;
+using P328Pustok.ViewModels;
+
+namespace P328Pustok.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class SliderController:Controller
+    {
+        private readonly PustokContext _context;
+
+        public SliderController(PustokContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(string search = null)
+        {
+            var query = _context.Sliders.AsQueryable();
+
+            if (search != null)
+                query = query.Where(x => x.Name.Contains(search));
+
+            ViewBag.Search = search;
+
+            return View(query.ToList());
+        }
+    }
+}
